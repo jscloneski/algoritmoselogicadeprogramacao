bf3a04eede6985edf6f0f97f6a854197ce054a12
João Scloneski <jscloneski.dev@gmail.com>
2022-11-28 19:22:38 -0300

    att

--- script.js
+++ script.js
@@ -31,21 +31,24 @@ while(option != 3) {
         3. Sair do programa.
     `))
 
-    if(option == 1) {
-        let item = prompt("Digite o nome do item")
-        items.push(item)
-    }
-
-    else if (option == 2) {
-
-        if(items.length == 0) {
+    switch(option) {
+        case 1:
+            let item = prompt("Digite o nome do item")
+            items.push(item)
+            break;
+        case 2:
+            if(items.length == 0) {
             alert("Não existem itens cadastrados")
         } else {
             alert(items)
         }
-
-    } else {
-        alert("Tchau")
+            break;
+        case 3:
+            alert("Tchau")
+            break;
+        default:
+            alert("Opção inválida, tente novamente.")
     }
+
 }
 
