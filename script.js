ec34800b4d0b7ad036fc1c5db0e4afc51f29e2f2
João Scloneski <jscloneski.dev@gmail.com>
2023-04-17 21:06:15 -0300

    cbk

---
+++ script.js
@@ -0,0 +1,39 @@
+/*  Crie uma lista de pacientes 
+
+    Cada paciente dentro da lista deverá conter 
+    
+        Nome
+        Idade
+        Peso
+        Altura
+        
+    Escreva uma lista contendo o nome dos pacientes*/
+
+const patients = [
+    {
+        name: "Luiz",
+        age: 20,
+        weight: 100,
+        height: 190,
+    },
+    {
+        name: "Alexandre",
+        age: 27,
+        weight: 70,
+        height: 170,
+    },
+    {
+        name: "Carlos",
+        age: 42,
+        weight: 90,
+        height: 180,
+    },
+]
+
+let patientsNames = []
+
+for(let patient of patients) {
+    patientsNames.push(patient.name)
+}
+
+alert(patientsNames)
